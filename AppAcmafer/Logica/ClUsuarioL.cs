40c63b2bf391a3a30055e095152884955b0b74b9
yulianarianoo-ux <yuliana.rianoo@colpre.edu.co>
2025-11-20 07:16:49 -0500

    Visualizar productos que estan en venta

---
+++ AppAcmafer/Logica/ClUsuarioL.cs
@@ -0,0 +1,35 @@
+﻿using AppAcmafer.Datos;
+using AppAcmafer.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppAcmafer.Logica
+{
+    public class ClUsuarioL
+    {
+         ClUsuarioD ClUsuarioD = new ClUsuarioD();
+
+        public List<ClUsuarioM> ListarUsuariosRegistrados(string rolSolicitante)
+        {
+            if (rolSolicitante != "Administrador")
+            {
+                Console.WriteLine("Acceso denegado. Se requiere rol de Administrador.");
+                return null;
+            }
+
+            var usuariosDB = ClUsuarioD.UsuariosRegistrados();
+
+            var listaFinal = usuariosDB.Select(u => new ClUsuarioM
+            {
+                Nombre = u.Nombre,
+                Email = u.Email,
+                Rol = u.Rol,
+                EstadoCuenta = u.EstadoCuenta
+            }).ToList();
+
+            return listaFinal;
+        }
+    }
+}
